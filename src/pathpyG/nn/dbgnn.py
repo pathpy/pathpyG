d908d127ba8f4c7b7a70e7a3864cc3bb3bae02f0
lisi <lisi.qarkaxhija@uni-wuerzburg.de>
2023-08-09 08:14:23 +0000

    added training and testing, still error

--- src/pathpyG/nn/dbgnn.py
+++ src/pathpyG/nn/dbgnn.py
@@ -1,27 +1,8 @@
 
 from __future__ import annotations
 
-from typing import (
-    Any,
-    Dict,
-    Set,
-    Optional,
-    Tuple,
-    Union,
-)
-
-import numpy as np
-from enum import Enum
-
-from torch import Tensor, IntTensor, cat, sum
-
-from collections import defaultdict
-
-from pathpyG import config
-
-import torch 
-import torch.nn import Linear, ModuleList
-import torch_geometric
+import torch
+from torch.nn import Linear, ModuleList, Module
 import torch.nn.functional as F
 from torch_geometric.nn import MessagePassing, GCNConv
 
@@ -35,35 +16,37 @@ class BipartiteGraphOperator(MessagePassing):
         x = (self.lin1(x[0]), self.lin2(x[1]))
         return self.propagate(bipartite_index, size=(N, M), x=x)
 
-class DBGNN(torch.nn.Module):
+class DBGNN(Module):
     """Implementation of time-aware graph neural network DBGNN
     Reference paper: https://openreview.net/pdf?id=Dbkqs1EhTr
-    
+
     Args:
-    num_classes: int - number of classes 
-    num_features: list - number of features for first order and higher order nodes, e.g. [first_order_num_features, second_order_num_features]
-    hidden_dims: list - number of hidden dimensions per each layer in the first/higher order network
-    p_dropout: float - drop-out probability
+        num_classes: int - number of classes
+        num_features: list - number of features for first order and higher order nodes, e.g. [first_order_num_features, second_order_num_features]
+        hidden_dims: list - number of hidden dimensions per each layer in the first/higher order network
+        p_dropout: float - drop-out probability
     """
     def __init__(
         self,
         num_classes,
         num_features,
         hidden_dims,
-        p_dropout = 0.4
+        p_dropout=0.0
         ):
         super().__init__()
-        
+
         self.num_features = num_features
         self.num_classes = num_classes
         self.hidden_dims = hidden_dims
         self.p_dropout = p_dropout
-        
+
         # higher-order layers
-        self.higher_order_layers = ModuleList(GCNConv(self.num_features[1], self.hidden_dims[0]))
+        self.higher_order_layers = ModuleList()
+        self.higher_order_layers.append(GCNConv(self.num_features[1], self.hidden_dims[0]))
 
         # first-order layers
-        self.first_order_layers = ModuleList(GCNConv(self.num_features[1], self.hidden_dims[0]))
+        self.first_order_layers = ModuleList()
+        self.first_order_layers.append(GCNConv(self.num_features[1], self.hidden_dims[0]))
 
         for dim in self.hidden_dims[1:-1]:
             # higher-order layers
@@ -72,12 +55,12 @@ class DBGNN(torch.nn.Module):
             self.first_order_layers.append(GCNConv(self.hidden_dims[dim - 1], self.hidden_dims[dim]))
 
         self.bipartite_layer = BipartiteGraphOperator(self.hidden_dims[-2], self.hidden_dims[-1])
-        
-        # mlp
+
+        # Linear layer
         self.lin = torch.nn.Linear(self.hidden_dims[-1], num_classes)
-        
-        
-    
+
+
+
     def forward(self, data):
 
         x = data.x
@@ -96,10 +79,10 @@ class DBGNN(torch.nn.Module):
         x_h = F.dropout(x_h, p=self.p_dropout, training=self.training)
 
         # Bipartite message passing
-        x = torch.nn.functional.elu(self.layer_2_1((x_h, x), data.bipartite_edge_index, N = data.num_ho_nodes, M= data.num_nodes))
+        x = torch.nn.functional.elu(self.bipartite_layer((x_h, x), data.bipartite_edge_index, N = data.num_ho_nodes, M= data.num_nodes))
         x = F.dropout(x, p=self.p_dropout, training=self.training)
 
         # Linear layer
         x = self.lin(x)
-            
+
         return x
