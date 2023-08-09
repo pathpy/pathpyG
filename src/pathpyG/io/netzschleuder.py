4d31b1c8eb4589b2df97a7fda27b69c17751e080
Ingo Scholtes <ingo.scholtes@uni-wuerzburg.de>
2023-08-09 16:05:24 +0000

    fised type error

--- src/pathpyG/io/netzschleuder.py
+++ src/pathpyG/io/netzschleuder.py
@@ -429,7 +429,7 @@ def read_netzschleuder_record(name: str, base_url: str='https://networks.skewed.
 
 def read_netzschleuder_network(name: str, net: Optional[str]=None,
         ignore_temporal: bool=False, multiedges: bool=False,
-        base_url: str='https://networks.skewed.de') -> Optional[Union[Graph, TemporalGraph]]:
+        base_url: str='https://networks.skewed.de') -> Union[Graph, TemporalGraph]:
     """Reads a pathpy network record from the netzschleuder repository.
 
     Parameters
