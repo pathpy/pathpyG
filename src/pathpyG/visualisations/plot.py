42b48a3bc961b81164e880638b25a1901b43c6d0
Jürgen Hackl <hackl@princeton.edu>
2023-10-24 11:32:32 -0400

    add function to load backends

--- src/pathpyG/visualisations/plot.py
+++ src/pathpyG/visualisations/plot.py
@@ -7,15 +7,57 @@
 #
 # Copyright (c) 2016-2019 Pathpy Developers
 # =============================================================================
+import os
 import logging
+import importlib
 
-# from typing import Any
+from typing import Optional, Any
+
+# from copy import deepcopy
 
 # create logger
 logger = logging.getLogger("root")
 
+# supported backends
+_backends: set = {"d3js", "tikz", "matplotlib"}
 
-# from copy import deepcopy
+# supported file formats
+_formats: dict = {
+    ".html": "d3js",
+    ".tex": "tikz",
+    ".pdf": "tikz",
+    ".png": "matplotlib",
+}
+
+
+def _get_plot_backend(
+    backend: Optional[str] = None,
+    filename: Optional[str] = None,
+    default: str = "d3js",
+) -> Any:
+    """Return the plotting backend to use."""
+    # use default backend per default
+    _backend: str = default
+
+    if isinstance(filename, str):
+        _backend = _formats.get(os.path.splitext(filename)[1], default)
+
+    # if no backend was found use the backend suggested for the file format
+    if backend is not None and backend not in _backends and filename is not None:
+        logger.error(f"The backend <{backend}> was not found.")
+        raise KeyError
+
+    # if no backend was given use the backend suggested for the file format
+    elif isinstance(backend, str) and backend in _backends:
+        _backend = backend
+
+    try:
+        module = importlib.import_module(f"pathpyG.visualisations._{_backend}")
+    except ImportError:
+        logger.error(f"The <{_backend}> backend could not be imported.")
+        raise ImportError from None
+
+    return module
 
 
 class PathPyPlot:
--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,8 +1,8 @@
-# from __future__ import annotations
-
-from pathpyG.visualisations.plot import PathPyPlot
+import pytest
 
 # import torch
+from pathpyG.visualisations.plot import PathPyPlot
+from pathpyG.visualisations.plot import _get_plot_backend
 
 
 def test_PathPyPlot() -> None:
@@ -11,3 +11,11 @@ def test_PathPyPlot() -> None:
 
     assert isinstance(plot.data, dict)
     assert isinstance(plot.config, dict)
+
+
+def test_get_plot_backend() -> None:
+    """Test to get a valid plot backend."""
+
+    # backend which does not exist
+    with pytest.raises(ImportError):
+        _get_plot_backend(default="does not exist")
