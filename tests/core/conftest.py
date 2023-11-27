4e47875af2aa70a506d35a18dc20270214d553c3
Ingo Scholtes <ingo.scholtes@uni-wuerzburg.de>
2023-11-27 22:41:28 +0000

    formatting issues

--- tests/core/conftest.py
+++ tests/core/conftest.py
@@ -4,23 +4,26 @@ import pytest
 import torch
 
 from pathpyG.core.Graph import Graph
-from pathpyG.core.TemporalGraph import TemporalGraph
 from pathpyG.core.Paths import PathData
+from pathpyG.core.TemporalGraph import TemporalGraph
+
 
 @pytest.fixture
 def simple_graph():
-    return Graph.from_edge_list([['a','b'], ['b','c'], ['a','c']])
+    return Graph.from_edge_list([["a", "b"], ["b", "c"], ["a", "c"]])
+
 
 @pytest.fixture
 def simple_paths():
     paths = PathData()
-    paths.add_walk(torch.tensor([[0,2],[2,3]])) # A -> C -> D
-    paths.add_walk(torch.tensor([[0,2],[2,3]])) # A -> C -> D
-    paths.add_walk(torch.tensor([[1,2],[2,4]])) # B -> C -> E
-    paths.add_walk(torch.tensor([[1,2],[2,4]])) # B -> C -> E
+    paths.add_walk(torch.tensor([[0, 2], [2, 3]]))  # A -> C -> D
+    paths.add_walk(torch.tensor([[0, 2], [2, 3]]))  # A -> C -> D
+    paths.add_walk(torch.tensor([[1, 2], [2, 4]]))  # B -> C -> E
+    paths.add_walk(torch.tensor([[1, 2], [2, 4]]))  # B -> C -> E
     return paths
+
 
 @pytest.fixture
 def simple_temporal_graph():
-    tedges = [('a', 'b', 1), ('b', 'c', 5), ('c', 'd', 9), ('c', 'e', 9)]
+    tedges = [("a", "b", 1), ("b", "c", 5), ("c", "d", 9), ("c", "e", 9)]
     return TemporalGraph.from_edge_list(tedges)
