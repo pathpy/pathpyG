9685f9783da4ebe153cdd6b992fc999495c47f12
Ingo Scholtes <ingo.scholtes@uni-wuerzburg.de>
2023-11-28 22:14:05 +0000

    fixes #37

--- src/pathpyG/core/HigherOrderGraph.py
+++ src/pathpyG/core/HigherOrderGraph.py
@@ -18,11 +18,17 @@ class HigherOrderGraph(Graph):
     def __init__(self, paths: PathData, order: int = 1, node_id: Any = None, **kwargs):
         """Generate HigherOrderGraph based on a given PathData instance.
 
-        Usage example:
+        Args:
+            paths: xxx
+            order: xxx
+            node_id: xxx
+            **kwargs: xxx
+
+        Usage Example:
             import pathpyG as pp
 
             paths = pp.PathData()
-            paths.add_walk(torch.Tensor([[0,1,2],[1,2,3]]))
+            paths.add_walk(torch.Tensor([[0, 1, 2], [1, 2, 3]]))
             g2 = Graph(paths, k=2, node_id=['a', 'b', 'c', 'd'])
         """
         if node_id is None:
@@ -40,15 +46,18 @@ class HigherOrderGraph(Graph):
             self._nodes = index.reshape(-1, index.size(dim=2)).unique(dim=0)
 
             # create mapping to first-order node indices
-            ho_nodes_to_index = {tuple(j.tolist()):i for i, j in enumerate(self._nodes)}
+            ho_nodes_to_index = {tuple(j.tolist()): i for i, j in enumerate(self._nodes)}
 
             # create new tensor with node indices mapped to indices of higher-order nodes
-            edge_index = torch.tensor( ([ho_nodes_to_index[tuple(x.tolist())] for x in index[0,:]], [ho_nodes_to_index[tuple(x.tolist())] for x in index[1,:]])).to(config['torch']['device'])
+            edge_index = torch.tensor( (
+                [ho_nodes_to_index[tuple(x.tolist())] for x in index[0,:]],
+                [ho_nodes_to_index[tuple(x.tolist())] for x in index[1,:]])
+                ).to(config['torch']['device'])
 
             # create mappings between higher-order nodes (with ids) and node indices
             if len(node_id)>0:
-                self.node_index_to_id = { i:tuple([node_id[v] for v in j.tolist()]) for i, j in enumerate(self._nodes)}
-                self.node_id_to_index = { j:i for i, j in self.node_index_to_id.items()}
+                self.node_index_to_id = { i: tuple([node_id[v] for v in j.tolist()]) for i, j in enumerate(self._nodes)}
+                self.node_id_to_index = { j: i for i, j in self.node_index_to_id.items()}
             else:
                 self.node_index_to_id = { i:tuple([v for v in j.tolist()]) for i, j in enumerate(self._nodes)}
                 self.node_id_to_index = { j:i for i, j in self.node_index_to_id.items()}
@@ -71,7 +80,9 @@ class HigherOrderGraph(Graph):
         self.edge_to_index = {(e[0].item(), e[1].item()):i for i, e in enumerate([e for e in edge_index.t()])}
 
         # initialize adjacency matrix
-        self._sparse_adj_matrix = torch_geometric.utils.to_scipy_sparse_matrix(self.data.edge_index).tocsr()
+        self._sparse_adj_matrix: Any = (
+            torch_geometric.utils.to_scipy_sparse_matrix(self.data.edge_index).tocsr()
+        )
 
     # @Graph.nodes.getter
     # def nodes(self):
@@ -94,10 +105,8 @@ class HigherOrderGraph(Graph):
 
 
 
-    def __str__(self):
-        """
-        Returns a string representation of the graph
-        """
+    def __str__(self) -> str:
+        """Return a string representation of the higher-order graph."""
 
         attr_types = Graph.attr_types(self.data.to_dict())
 
--- tests/core/test_higher_order_graph.py
+++ tests/core/test_higher_order_graph.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 
 from pathpyG.algorithms.temporal import temporal_graph_to_event_dag
 from pathpyG.core.HigherOrderGraph import HigherOrderGraph
+from pathpyG.core.TemporalGraph import TemporalGraph
 from pathpyG.core.PathData import PathData
 
 
-def test_higher_order_from_temporal_graph(simple_temporal_graph):
+def test_higher_order_from_temporal_graph(simple_temporal_graph: TemporalGraph):
     dag = temporal_graph_to_event_dag(simple_temporal_graph, delta=5, sparsify=True)
     paths = PathData.from_temporal_dag(dag, detect_walks=False)
 
@@ -27,24 +28,24 @@ def test_higher_order_from_temporal_graph(simple_temporal_graph):
     assert g3.M == 2
     assert g3.data.edge_weight.sum() == 2.0
 
-def test_successors(simple_paths):
+def test_successors(simple_paths: PathData):
     g2 = HigherOrderGraph(simple_paths, order=2,
                           node_id=['A', 'B', 'C', 'D', 'E'])
 
-    assert ('C', 'D') in list(g2.successors(('A', 'C')))
-    assert ('C', 'E') in list(g2.successors(('B', 'C')))
+    assert set([('C', 'D')]) == set(g2.successors(('A', 'C')))
+    assert set([('C', 'E')]) == set(g2.successors(('B', 'C')))
 
-def test_predecessors(simple_paths):
+def test_predecessors(simple_paths: PathData):
     g2 = HigherOrderGraph(simple_paths, order=2,
                           node_id=['A', 'B', 'C', 'D', 'E'])
 
-    assert ('A', 'C') in list(g2.predecessors(('C', 'D')))
-    assert ('B', 'C') in list(g2.predecessors(('C', 'E')))
+    assert set([('A', 'C')]) == set(g2.predecessors(('C', 'D')))
+    assert set([('B', 'C')]) == set(g2.predecessors(('C', 'E')))
 
-def test_higher_order_graph(simple_paths):
+def test_higher_order_graph(simple_paths: PathData):
     g2 = HigherOrderGraph(simple_paths, order=2,
                         node_id=['A', 'B', 'C', 'D', 'E'])
-    assert g2.N == 4
-    assert g2.M == 4
     assert set(g2.nodes) == set([('A', 'C'), ('C', 'D'), ('B', 'C'), ('C', 'E')])
     assert set(g2.edges) == set([(('A', 'C'), ('C', 'D')), (('B', 'C'), ('C', 'E'))])
+    assert g2.N == 4
+    assert g2.M == 2
