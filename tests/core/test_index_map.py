dd436bc87d202020c89747a04f8d663e1ac55bb4
Ingo Scholtes <ingo.scholtes@uni-wuerzburg.de>
2024-01-25 15:29:20 +0000

    added test for HigherOrderIndexMap

--- tests/core/test_index_map.py
+++ tests/core/test_index_map.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
 from pathpyG.core.IndexMap import IndexMap
-
+from pathpyG.core.HigherOrderIndexMap import HigherOrderIndexMap
+from pathpyG.core.PathData import PathData
+from pathpyG.core.HigherOrderGraph import HigherOrderGraph
 
 def test_index_mapping():
     mapping = IndexMap()
@@ -30,3 +32,13 @@ def test_index_mapping():
     assert mapping.to_id(1) == 'c'
     assert mapping.num_ids() == 2
     assert mapping.node_ids == ['a', 'c']
+
+def test_higher_order_index_mapping(simple_paths):
+    g2 = HigherOrderGraph(simple_paths, order=2,
+                          node_ids=['A', 'B', 'C', 'D', 'E'])
+    assert g2.mapping.to_idx(('A', 'C')) == 0
+    assert g2.mapping.to_id(1) == ('B', 'C')
+
+    g2 = HigherOrderGraph(simple_paths, order=2)
+    assert g2.mapping.to_idx(0) == 0
+    assert g2.mapping.to_id(1) == 1
