b62a3d0b34dde03fc65185c2a1cb2768e1d2fec9
Ingo Scholtes <ingo.scholtes@uni-wuerzburg.de>
2024-01-26 14:50:24 +0000

    documentation

--- src/pathpyG/nn/dbgnn.py
+++ src/pathpyG/nn/dbgnn.py
@@ -90,12 +90,9 @@ class DBGNN(Module):
     
     @staticmethod
     def generate_bipartite_edge_index(g: HigherOrderGraph, g2: HigherOrderGraph, mapping: str = 'last') -> torch.Tensor:
+        """Generate edge_index for bipartite graph connecting nodes of a second-order graph to first-order nodes."""
 
         if mapping == 'last':
-            # bipartide_edge_index = torch.tensor(
-            #     [list(g2.node_index_to_id.keys()),
-            #     [g.node_id_to_index[i[1]] for i in g2.node_index_to_id.values()]]
-            #     )
             bipartide_edge_index = torch.tensor(
                 [list(range(g2.N)),
                 [g.mapping.to_idx(i[1]) for i in g2.mapping.idx_to_id.values()]]
