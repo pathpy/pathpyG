ecd32b02424abcaf443ada650c07e42e92eaa045
Ingo Scholtes <ingo.scholtes@uni-wuerzburg.de>
2024-01-29 16:46:57 +0000

    adding benchmarks

---
+++ tests/benchmark/test_benchmark.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+import pytest
+import torch
+
+from pathpyG.core.Graph import Graph
+from pathpyG.core.PathData import PathData
+from pathpyG import config
+from pathpyG.core.HigherOrderGraph import HigherOrderGraph
+
+def construct_higher_order(max_order):
+    paths = PathData.from_csv('docs/data/tube_paths_train.ngram')
+    for k in range(1, max_order):
+        gk = HigherOrderGraph(paths, order=k, path_freq='path_freq')
+
+@pytest.mark.benchmark
+def test_higher_order_gpu(benchmark):
+
+    config['torch']['device'] = 'cuda'
+    benchmark.pedantic(construct_higher_order, kwargs={'max_order': 5}, iterations=1, rounds=2)
+
+@pytest.mark.benchmark
+def test_higher_order_cpu(benchmark):
+
+    config['torch']['device'] = 'cpu'
+    benchmark.pedantic(construct_higher_order, kwargs={'max_order': 5}, iterations=1, rounds=2)
