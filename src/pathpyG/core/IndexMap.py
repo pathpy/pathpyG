b35100b534a481f3125b9920e44ca0404b528ebb
Moritz Lampert <moritz@lampert.ai>
2024-02-27 09:29:25 +0000

    sort index map tests

--- src/pathpyG/core/IndexMap.py
+++ src/pathpyG/core/IndexMap.py
@@ -4,14 +4,18 @@ from typing import TYPE_CHECKING, Dict, List, Union
 
 class IndexMap:
     """Maps node indices to string ids"""
+
+    id_to_idx: Dict[str, int]
+    idx_to_id: Dict[int, str]
+
     def __init__(self, node_ids: Union[List[str], None] = None) -> None:
         """Initialize mapping from indices to node IDs."""
         if node_ids is not None:
             assert len(node_ids) == len(set(node_ids)), "node_id entries must be unique"
             self.node_ids = node_ids
-            # first-order: nodes = integer indices 
-            self.idx_to_id: Dict[int, str] = dict(enumerate(node_ids))
-            self.id_to_idx: Dict[str, int] = {v: i for i, v in enumerate(node_ids)}
+            # first-order: nodes = integer indices
+            self.idx_to_id = dict(enumerate(node_ids))
+            self.id_to_idx = {v: i for i, v in enumerate(node_ids)}
             # higher-order: nodes = tensors of integer indices
             # _nodes = list of unique higher-order nodes as tensors
             # self.idx_to_id = { i: tuple([node_ids[v] for v in j.tolist()]) for i, j in enumerate(self._nodes)}
@@ -23,34 +27,33 @@ class IndexMap:
             self.id_to_idx = {}
             self.has_ids = False
 
-    def num_ids(self):
+    def num_ids(self) -> int:
+        """Return number of node IDs."""
         return len(self.node_ids)
 
-    def add_id(self, id):
+    def add_id(self, node_id: str) -> None:
         """Assigns additional ID to next consecutive index."""
         if id not in self.id_to_idx:
             idx = self.num_ids()
-            self.node_ids.append(id)
-            self.idx_to_id[idx] = id
-            self.id_to_idx[id] = idx
+            self.node_ids.append(node_id)
+            self.idx_to_id[idx] = node_id
+            self.id_to_idx[node_id] = idx
             self.has_ids = True
 
     def to_id(self, idx: int) -> Union[int, str]:
         """Map index to ID if mapping is defined, return index otherwise."""
         if self.has_ids:
             return self.idx_to_id[idx]
-        else:
-            return idx
+        return idx
 
     def to_idx(self, node: Union[str, int]) -> int:
         """Map argument (ID or index) to index if mapping is defined, return argument otherwise."""
         if self.has_ids:
             return self.id_to_idx[node]
-        else:
-            return node
+        return node
 
     def __str__(self) -> str:
-        s = ''
+        s = ""
         for v in self.id_to_idx:
-            s += str(v) + ' -> ' + str(self.to_idx(v)) + '\n'
+            s += str(v) + " -> " + str(self.to_idx(v)) + "\n"
         return s
--- tests/core/test_index_map.py
+++ tests/core/test_index_map.py
@@ -1,39 +1,34 @@
+# pylint: disable=missing-function-docstring,missing-module-docstring
+
 from __future__ import annotations
 
 from pathpyG.core.IndexMap import IndexMap
-from pathpyG.core.HigherOrderIndexMap import HigherOrderIndexMap
-from pathpyG.core.PathData import PathData
-from pathpyG.core.HigherOrderGraph import HigherOrderGraph
+
 
 def test_index_mapping():
     mapping = IndexMap()
 
     assert mapping.to_idx(0) == 0
     assert mapping.to_idx(42) == 42
-    
+
     assert mapping.to_id(0) == 0
     assert mapping.to_id(42) == 42
 
-    mapping.add_id('a')
+    mapping.add_id("a")
 
-    assert mapping.to_idx('a') == 0
-    assert mapping.to_id(0) == 'a'
+    assert mapping.to_idx("a") == 0
+    assert mapping.to_id(0) == "a"
     assert mapping.num_ids() == 1
-    assert mapping.node_ids == ['a']
+    assert mapping.node_ids == ["a"]
 
-    mapping.add_id('a')
+    mapping.add_id("a")
 
     assert mapping.num_ids() == 1
-    assert mapping.node_ids == ['a']
+    assert mapping.node_ids == ["a"]
 
-    mapping.add_id('c')
+    mapping.add_id("c")
 
-    assert mapping.to_idx('c') == 1
-    assert mapping.to_id(1) == 'c'
+    assert mapping.to_idx("c") == 1
+    assert mapping.to_id(1) == "c"
     assert mapping.num_ids() == 2
-    assert mapping.node_ids == ['a', 'c']
-
-def test_higher_order_index_mapping(simple_walks):
-    g2 = HigherOrderGraph(simple_walks, order=2)
-    assert g2.mapping.to_idx(('A', 'C')) == 0
-    assert g2.mapping.to_id(1) == ('B', 'C')
+    assert mapping.node_ids == ["a", "c"]
