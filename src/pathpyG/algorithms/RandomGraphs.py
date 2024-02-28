35e8788dfe489b420a9d4da8cd16abf266836fee
Lisi <lisqarkaxhija@gmail.com>
2024-02-28 15:21:25 +0100

    reimplemented the code

--- src/pathpyG/algorithms/RandomGraphs.py
+++ src/pathpyG/algorithms/RandomGraphs.py
@@ -2,94 +2,57 @@
 
 from typing import Optional, Union, Dict
 
-import numpy as np
-from tqdm.notebook import tqdm
+import torch
 
 import pathpyG as pp
-from pathpyG import Graph
 
 
 
 
 def Watts_Strogatz(n: int, s: int, p: float = 0.0, loops: bool = False,
-                mapping: list = None) -> pp.Graph:
-    """Undirected Watts-Strogatz lattice network
-
-    Generates an undirected Watts-Strogatz lattice network with lattice
-    dimensionality one.
-
+                mapping: pp.IndexMap = None) -> pp.Graph:
+    """Generate a Watts-Strogatz small-world graph.
     Parameters
     ----------
     n : int
-
-        The number of nodes in the generated network
-
-    s : float
-
-        The number of nearest neighbors that will be connected
-        in the ring lattice
-
+        The number of nodes in the graph.
+    s : int
+        The number of edges to attach from a new node to existing nodes.
     p : float
+        The probability of rewiring each edge.
+    loops : bool
+        If True, allow the graph to contain self-loops.
+    mapping : IndexMap
+        A mapping from the node indices to node names.
+    Returns
+    -------
+    Graph
+        A Watts-Strogatz small-world graph.
 
-        The rewiring probability
-
-    mapping : list, optional
-
-        Optional mapping of node indices to string IDs
-
-    Examples
-    --------
-    Generate random undirected network with 10 nodes
-
-    >>> import pathpy as pp
-    >>> random_graph = pp.algorithms.random_graphs.Watts_Strogatz(n=50, s=4, p=0.01, mapping=[str(x) for x in range(50)])
-    >>> print(random_graph.summary())
-    ...
-
+    Example
+    -------
+    >>> g = Watts_Strogatz(100, 4, 0.1, mapping=pp.IndexMap([f"n_{i}" for i in range(100)])
+    >>> pp.visualisations.plot(g, node_label=g.mapping.node_ids)
     """
-    
-    nodes = []
 
-    if mapping is None or len(mapping) != n:
-        mapping = {}
-        for i in range(n):
-            nodes.append(str(i))
-            mapping[str(i)] = i
-        reversed_mapping = {
-            j: i for i,j in mapping.items()
-        }
-    else:
-        mapping = {
-        j: i for i,j in enumerate(mapping)
-        }
-        reversed_mapping = {
-            i: j for i,j in enumerate(mapping)
-        }
-        for i in range(n):
-            nodes.append(reversed_mapping[i])
+    nodes = torch.arange(n)
 
     # construct a ring lattice (dimension 1)
-    nodes = torch.arange(s)
-    edges = IntTensor([nodes[:-1], nodes[1:]])
+    edges_right = torch.stack([torch.stack((nodes, torch.roll(nodes, shifts=-i, dims=0))) for i in range(1, s+1)], dim=0).permute(1, 0, 2).reshape(2, -1)
+    edges_left = torch.stack([torch.stack((nodes, torch.roll(nodes, shifts=+i, dims=0))) for i in range(1, s+1)], dim=0).permute(1, 0, 2).reshape(2, -1)
+    edges = torch.cat((edges_right, edges_left), dim=1)
 
     if p == 0:
-        # nothing to do here
         return pp.Graph()
 
     # Rewire each link with probability p
-    new_edges = []
-    rnd_edge_mask = (torch.rand(edges.size(1) < p)
-    rnd_edges = edges[rnd_edge_mask]
-    rnd_edges[1] = torch.randint(len(nodes), (rnd_edges.size(1),)
-    edges[rnd_edge_mask] = rnd_edges
-    while edges.size(1) != edges.unique(dim=0).size(1):
-       unique_edges = edges.unique(return_inverse=True, dim=0)[1]
-       mask = torch.ones(edges.size(1), dtype=bool)
-       mask[unique_edges] = False
-       doubled_edges = edges[mask]
-       doubled_edges[1] =  torch.randint(len(nodes), (doubled_edges.size(1),)
-       edges[mask] = doubled_edges
+    rand_vals = torch.rand(edges.shape[0])
+    rewire_mask = rand_vals < p
 
-    g = pp.Graph.from_edge_list(new_edges)
+    new_edges = edges.clone()
+    new_edges[rewire_mask, 1] = torch.randint(n, (torch.sum(rewire_mask),)).squeeze()
+
+    g = pp.Graph.from_edge_index(new_edges.tolist(), mapping=mapping)
+
     g = g.to_undirected()
     return g
