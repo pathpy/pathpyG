c0ed541a949cfad3862b1ebeaec23755a4e4ec2b
Moritz Lampert <moritz@lampert.ai>
2024-03-25 17:54:27 +0100

    WIP (#140)

--- tests/core/test_index_map.py
+++ tests/core/test_index_map.py
@@ -1,9 +1,6 @@
 from __future__ import annotations
 
 from pathpyG.core.IndexMap import IndexMap
-from pathpyG.core.HigherOrderIndexMap import HigherOrderIndexMap
-from pathpyG.core.PathData import PathData
-from pathpyG.core.HigherOrderGraph import HigherOrderGraph
 
 def test_index_mapping():
     mapping = IndexMap()
@@ -32,8 +29,3 @@ def test_index_mapping():
     assert mapping.to_id(1) == 'c'
     assert mapping.num_ids() == 2
     assert mapping.node_ids == ['a', 'c']
-
-def test_higher_order_index_mapping(simple_walks):
-    g2 = HigherOrderGraph(simple_walks, order=2)
-    assert g2.mapping.to_idx(('A', 'C')) == 0
-    assert g2.mapping.to_id(1) == ('B', 'C')
