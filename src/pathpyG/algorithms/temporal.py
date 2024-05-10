fixed issue in temporal shortest path calculation

src/pathpyG/algorithms/temporal.py
@@ -39,7 +39,7 @@ def lift_order_temporal(g: TemporalGraph, delta: int = 1):
 
         # find indices of all edges that can possibly continue edges occurring at time t for the given delta
         dst_time_mask = (timestamps > t) & (timestamps <= t+delta)
-        dst_edges = edge_index[:,dst_time_mask]        
+        dst_edges = edge_index[:,dst_time_mask]
         dst_edge_idx = indices[dst_time_mask]
 
         if dst_edge_idx.size(0)>0 and src_edge_idx.size(0)>0:
@@ -58,11 +58,11 @@ def temporal_shortest_paths(g: TemporalGraph, delta: int):
     # generate temporal event DAG
     edge_index = lift_order_temporal(g, delta)
 
-    # Add indices of first-order nodes as src and dst of paths in augmented
-    # temporal event DAG
+    # Add indices of g.N first-order nodes as source nodes of paths in augmented TEG
     src_edges_src = g.data.edge_index[0] + g.M
     src_edges_dst = torch.arange(0, g.data.edge_index.size(1))
 
+    # Add indices of g.N first-order nodes as target nodes of paths in augmented TEG
     dst_edges_src = torch.arange(0, g.data.edge_index.size(1))
     dst_edges_dst = g.data.edge_index[1] + g.M + g.N
 
@@ -72,16 +72,16 @@ def temporal_shortest_paths(g: TemporalGraph, delta: int):
     edge_index = torch.cat([edge_index, src_edges, dst_edges], dim=1)
 
     # create sparse scipy matrix
-    event_graph = Graph.from_edge_index(edge_index) 
+    event_graph = Graph.from_edge_index(edge_index, num_nodes=g.M + 2 * g.N) 
     m = event_graph.get_sparse_adj_matrix()
 
     print(f"Created temporal event DAG with {event_graph.N} nodes and {event_graph.M} edges")
 
     # run disjktra for all source nodes
     dist, pred = dijkstra(m, directed=True, indices=np.arange(g.M, g.M+g.N),  return_predecessors=True, unweighted=True)
-    
+
     # limit to first-order destinations and correct distances
-    dist_fo = dist[:, g.M+g.N:] - 1
+    dist_fo = dist[:, g.M+g.N:] - 1    
     pred_fo = pred[:, g.N+g.M:]
     np.fill_diagonal(dist_fo, 0)
 