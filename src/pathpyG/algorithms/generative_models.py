3f619a1b4dbbee95012018c6ab40d419e2c48b25
Ingo Scholtes <ingo.scholtes@uni-wuerzburg.de>
2024-05-16 12:26:19 +0000

    added max_edges and is_graphic_sequence

--- src/pathpyG/algorithms/generative_models.py
+++ src/pathpyG/algorithms/generative_models.py
@@ -32,6 +32,41 @@ from torch_geometric.utils import to_networkx, degree
 from pathpyG.core.Graph import Graph
 from pathpyG.core.IndexMap import IndexMap
 
+def max_edges(n: int, directed: bool = False, multi_edges: bool = False, self_loops: bool = False) -> int | float:
+    """Returns the maximum number of edges that a directed or undirected network with n nodes can
+    possible have (with or without loops).
+
+    Args:
+        n: The number of nodes in the network  
+        directed: If True, return the maximum number of edges in a directed network.
+        multi_edges: If True, multiple edges between each node pair are allowed. In this case np.inf is returned.
+        self_loops: If True, include self-loops.
+
+    Example:
+    ```py
+        # Compute maximum number of edges in directed/undirected network with/without self-loops and 100 nodes
+        import pathpyG as pp
+        print(pp.algorithms.generative_models.max_edges(100)
+        # 4950
+
+        print(pp.algorithms.generative_models.max_edges(100, directed=True)
+        9900
+
+        print(pp.algorithms.generative_models.max_edges(100, directed=True, loops=True)
+        # 10000
+    ```
+    """
+
+    if multi_edges:
+        return _np.inf
+    elif self_loops and directed:
+        return int(n**2)
+    elif self_loops and not directed:
+        return int(n*(n+1)/2)
+    elif not self_loops and not directed:
+        return int(n*(n-1)/2)
+    else:  # not loops and directed:
+        return int(n*(n-1))
 
 def G_nm(n: int, m: int, mapping: IndexMap | None = None, self_loops: bool = False, multi_edges: bool = False, directed: bool = False) -> Graph:
     """Generate a random graph with n nodes and m edges based on the G(n,m) model by Pal Eröds and Alfred Renyi.
@@ -100,3 +135,27 @@ def G_np(n: int, p: float, mapping: IndexMap | None = None, self_loops: bool = F
         
     return Graph.from_edge_list(list(edges), is_undirected=not directed, mapping=mapping, num_nodes=n)
 
+def is_graphic_Erdos_Gallai(degrees: list[int]) -> bool:
+    """Check Erdös and Gallai condition.
+
+    Checks whether the condition by Erdös and Gallai (1967) for a graphic degree
+    sequence is fulfilled.
+
+    Args:
+        degrees: List of integer node degrees to be tested.
+    """
+    degree_sequence = sorted(degrees, reverse=True)
+    S = sum(degree_sequence)
+    n = len(degree_sequence)
+    if S % 2 != 0:
+        return False
+    for r in range(1, n):
+        M = 0
+        S = 0
+        for i in range(1, r+1):
+            S += degree_sequence[i-1]
+        for i in range(r+1, n+1):
+            M += min(r, degree_sequence[i-1])
+        if S > r * (r-1) + M:
+            return False
+    return True
--- tests/algorithms/test_generative_models.py
+++ tests/algorithms/test_generative_models.py
@@ -1,6 +1,8 @@
+import numpy as _np
+
 from pathpyG.core.Graph import Graph
 from pathpyG.core.IndexMap import IndexMap
-from pathpyG.algorithms.generative_models import G_nm, G_np
+from pathpyG.algorithms.generative_models import G_nm, G_np, is_graphic_Erdos_Gallai, max_edges
 
 
 def test_G_nm():
@@ -14,7 +16,7 @@ def test_G_nm():
     assert m_1.M == 2 * m
     # no multiple edges
     assert len(set([(v, w) for v, w in m_1.edges])) == len([(v, w) for v, w in m_1.edges])
-    assert m_1.is_directed() == False
+    assert m_1.is_directed() is False
 
     # test undirected graph w/o multi-edges, w/o self-loops and with custom IDs
     m_2 = G_nm(n=n, m=m, mapping=IndexMap([str(i) for i in range(n)]))
@@ -23,7 +25,7 @@ def test_G_nm():
     assert m_2.M == 2 * m
     # no multiple edges
     assert len(set([(v, w) for v, w in m_2.edges])) == len([(v, w) for v, w in m_2.edges])
-    assert m_2.is_directed() == False
+    assert m_2.is_directed() is False
 
     # test directed graph w/o multi-edges, w/o self-loops
     m_3 = G_nm(n=n, m=m, directed=True)
@@ -31,7 +33,7 @@ def test_G_nm():
     # 200 directed edges
     assert m_3.M == m
     assert len(set([(v, w) for v, w in m_3.edges])) == len([(v, w) for v, w in m_3.edges])
-    assert m_3.is_directed() == True
+    assert m_3.is_directed() is True
 
     # test undirected graph w/o multi-edges and with self-loops
     m_4 = G_nm(n=n, m=m, self_loops=True)
@@ -39,7 +41,7 @@ def test_G_nm():
     # since self-loops only exist in one direction we have 2 * m - n <= M <= 2 * m
     assert m_4.M >= 2*m - n and m_4.M <= 2 * m
     assert len(set([(v, w) for v, w in m_4.edges])) == len([(v, w) for v, w in m_4.edges])
-    assert m_4.is_directed() == False
+    assert m_4.is_directed() is False
 
 
 def test_G_np():
@@ -48,26 +50,41 @@ def test_G_np():
     n = 100
     p = 0.001
     m_1 = G_np(n=n, p=p)
-    assert m_1.N == n    
+    assert m_1.N == n
     # no multiple edges
     assert len(set([(v, w) for v, w in m_1.edges])) == len([(v, w) for v, w in m_1.edges])
-    assert m_1.is_directed() == False
+    assert m_1.is_directed() is False
+    assert m_1.M/2 <= max_edges(n)
 
     # test undirected graph w/o multi-edges, w/o self-loops and with custom IDs
     m_2 = G_np(n=n, p=p, mapping=IndexMap([str(i) for i in range(n)]))
     assert m_2.N == n
     # no multiple edges
     assert len(set([(v, w) for v, w in m_2.edges])) == len([(v, w) for v, w in m_2.edges])
-    assert m_2.is_directed() == False
+    assert m_2.is_directed() is False
+    assert m_2.M/2 <= max_edges(n)
 
     # test directed graph w/o multi-edges, w/o self-loops
     m_3 = G_np(n=n, p=p, directed=True)
     assert m_3.N == n    
     assert len(set([(v, w) for v, w in m_3.edges])) == len([(v, w) for v, w in m_3.edges])
-    assert m_3.is_directed() == True
+    assert m_3.is_directed() is True
+    assert m_3.M <= max_edges(n, directed=True)
 
     # test undirected graph w/o multi-edges and with self-loops
     m_4 = G_np(n=n, p=p, self_loops=True)
     assert m_4.N == n
     assert len(set([(v, w) for v, w in m_4.edges])) == len([(v, w) for v, w in m_4.edges])    
-    assert m_4.is_directed() == False
+    assert m_4.is_directed() is False
+    assert m_4.M <= max_edges(n, directed=False, self_loops=True)
+
+def test_max_edges():
+        assert max_edges(100) == 4950
+        assert max_edges(100, directed=True) == 9900
+        assert max_edges(100, directed=True, self_loops=True) == 10000
+        assert max_edges(100, directed=True, self_loops=True, multi_edges=True) == _np.inf
+
+def test_graphic_sequence():
+    assert is_graphic_Erdos_Gallai([1,0]) is False
+    assert is_graphic_Erdos_Gallai([1,3]) is False
+    assert is_graphic_Erdos_Gallai([1,1]) is True
