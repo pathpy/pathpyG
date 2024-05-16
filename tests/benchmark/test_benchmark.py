fdfe2f42dc137ce9ca8544b95f7d06306ef32985
Ingo Scholtes <ingo.scholtes@uni-wuerzburg.de>
2024-05-16 16:41:03 +0000

    added benchmark for temporal graphs

--- tests/benchmark/test_benchmark.py
+++ tests/benchmark/test_benchmark.py
@@ -4,22 +4,50 @@ import pytest
 import torch
 
 from pathpyG.core.Graph import Graph
+from pathpyG.core.TemporalGraph import TemporalGraph
 from pathpyG.core.path_data import PathData
 from pathpyG import config
 from pathpyG.core.MultiOrderModel import MultiOrderModel
 
-def construct_higher_order(max_order):
+# to run benchmarks, do the following:
+# > pip install pytest-benchmark
+# > run pytest -m benchmark
+
+
+def higher_order_paths(max_order):
     paths = PathData.from_ngram('docs/data/tube_paths_train.ngram')
     m = MultiOrderModel.from_PathData(paths, max_order=max_order)
 
+
+def higher_order_temporal_graph(max_order):
+    t = TemporalGraph.from_csv('docs/data/ants_1_1.tedges')
+    m = MultiOrderModel.from_temporal_graph(t, delta=30, max_order=max_order)
+
+
 @pytest.mark.benchmark
-def test_higher_order_gpu(benchmark):
+def test_higher_order_paths_gpu(benchmark):
 
     config['torch']['device'] = 'cuda'
-    benchmark.pedantic(construct_higher_order, kwargs={'max_order': 10}, iterations=1, rounds=2)
+    benchmark.pedantic(higher_order_paths, kwargs={'max_order': 10}, iterations=1, rounds=2)
+
 
 @pytest.mark.benchmark
-def test_higher_order_cpu(benchmark):
+def test_higher_order_paths_cpu(benchmark):
 
     config['torch']['device'] = 'cpu'
-    benchmark.pedantic(construct_higher_order, kwargs={'max_order': 10}, iterations=1, rounds=2)
+    benchmark.pedantic(higher_order_paths, kwargs={'max_order': 10}, iterations=1, rounds=2)
+
+
+@pytest.mark.benchmark
+def test_higher_order_temporal_graph_gpu(benchmark):
+
+    config['torch']['device'] = 'cuda'
+    benchmark.pedantic(higher_order_temporal_graph, kwargs={'max_order': 5}, iterations=1, rounds=10)
+
+
+@pytest.mark.benchmark
+def test_higher_order_temporal_graph_cpu(benchmark):
+
+    config['torch']['device'] = 'cpu'
+    benchmark.pedantic(higher_order_temporal_graph, kwargs={'max_order': 5}, iterations=1, rounds=10)
+
