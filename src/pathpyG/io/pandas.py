427f175c160c9e32bc99b937dfe2b57f1e376ea5
Ingo Scholtes <ingo.scholtes@uni-wuerzburg.de>
2024-09-10 18:23:29 +0000

    reimplemented pandas support

--- src/pathpyG/io/pandas.py
+++ src/pathpyG/io/pandas.py
@@ -28,80 +28,67 @@ def _check_column_name(frame: pd.DataFrame, name: str,
             if col in synonyms:
                 frame.rename(columns={col: name}, inplace=True)
                 continue
-
     return frame
 
 
-def df_to_graph(df: pd.DataFrame, loops: bool = True, directed: bool = True,
-               multiedges: bool = False, **kwargs: Any) -> Graph:
+def df_to_graph(df: pd.DataFrame, 
+                is_undirected: bool = False,
+                multiedges: bool = False, 
+                **kwargs: Any) -> Graph:
     """Reads a network from a pandas data frame.
 
-    The data frame is expected to have a minimum of two columns `v` and `w`
+    The data frame is expected to have a minimum of two columns
     that give the source and target nodes of edges. Additional columns in the
     data frame will be mapped to edge attributes.
 
-    Parameters
-    ----------
+    Args:
 
-    df: pandas.DataFrame
+        df: pandas.DataFrame
 
-        A data frame with rows containing edges and optional edge attributes.
+            A data frame with rows containing edges and optional edge attributes. If the 
+            data frame contains column names, the source and target columns must be called 
+            'v' and 'w' respectively. If no column names are used the first two columns 
+            are interpreted as source and target.
 
-    loops: Optional[bool]=True
+        is_udirected: Optional[bool]=True
 
-        Whether or not to add self-loops, i.e. rows in the data frame where
-        columns `v` and `w` (or configured synonyms) are identical. Default is
-        True.
+            whether or not to interpret edges as undirected
 
-    directed: Optional[bool]=True
+        multiedges: Optional[bool]=False
 
-        Whether or not to add edges as directed edges.
+            whether or not to allow multiple edges between the same node pair. By
+            default multi edges are ignored.
 
-    multiedges: Optional[bool]=False
+    Example:
+        ```py
 
-        Whether or not to allow multiple edges between the same node pair. By
-        default multi edges are ignored.
+        import pathpyG as pp
+        import pandas as pd
 
-    **kwargs: Any
-
-        Arbitrary keyword arguments that will be set as network-level
-        attributes.
-
-    Returns
-    -------
-    Graph
-
-        a static network object
-
-
-    Examples
-    --------
-
-    Create simple data frame and convert into network
-
-    >>> import pathpyG as pp
-    >>> import pandas as pd
-    >>> df = pd.DataFrame({
-    ...     'v': ['a', 'b', 'c'],
-    ...     'w': ['b', 'c', 'a'],
-    ...     'color': ['red', 'green', 'blue']})
-    >>> n = pp.io.df_to_graph(df, name='pandasGraph')
-    >>> print(n)
+        df = pd.DataFrame({
+            'v': ['a', 'b', 'c'],
+            'w': ['b', 'c', 'a'],
+            'edge_weight': [1.0, 5.0, 2.0]
+            })
+        g = pp.io.df_to_graph(df)
+        print(n)
+        ```
     """
+
+    # assign column names if no header is present
+    no_header = all(isinstance(x, int) for x in df.columns.values.tolist())
+
+    if no_header:
+        # interpret first two columns as source and target
+        col_names = ['v', 'w']
+        # interpret remaining columns as edge attributes
+        for i in range(2, len(df.columns.values.tolist())):
+            col_names += ['edge_attr_{0}'.format(i-2)]
+        df.columns = col_names
 
     df['v'] = df['v'].astype(str)
     df['w'] = df['w'].astype(str)
 
-    # create a set of all node uids
-    v_set = set(df['v'])
-    w_set = set(df['w'])
-    node_set: set = set(v_set).union(w_set)
-
-    if None in node_set:
-        print('DataFrame minimally needs columns \'v\' and \'w\'')
-        raise IOError
-
-    # initialize variables
     edges: list = []
     edge_set: set = set()
 
@@ -114,24 +101,25 @@ def df_to_graph(df: pd.DataFrame, loops: bool = True, directed: bool = True,
         # check if edge was already generated
         if (_v, _w) in edge_set and not multiedges:
             counter[(_v, _w)] += 1
-
-        elif loops or _v != _w:
-            # generate edge object
+        else:
+            # add edge
             edges.append((_v, _w))
             edge_set.add((_v, _w))
-        else:
-            continue
 
     # check for multi-edges
     if len(counter) > 0:
         print('%i edges existed already '
-              'and were not be considered. '
+              'and were not considered. '
               'To capture those edges, consider creating '
               'a multiedge and/or directed network.', sum(counter.values()))
 
     # create graph
-    g = Graph.from_edge_list(edges, is_undirected=not directed)
+    g = Graph.from_edge_list(edges, is_undirected=is_undirected)
+
+    # assign edge attributes
+    add_edge_attributes(df, g)
     return g
+
 
 def add_node_attributes(df: pd.DataFrame, g: Graph):
     """Add node attributes from pandas data frame to existing graph, where node
@@ -140,9 +128,19 @@ def add_node_attributes(df: pd.DataFrame, g: Graph):
     if 'v' not in df:
         print('data frame must have a column `v`')
         return
-    if df.duplicated(subset=['v']).any():
+    
+    attributed_nodes = list(df['v'])
+
+    # check for duplicated node attributes
+    if len(set(attributed_nodes)) < len(attributed_nodes):
         print('data frame cannot contain multiple attribute values for single node')
         return
+    
+    # check for difference between nodes in graph and nodes in attributes
+    if set(attributed_nodes) != set([v for v in g.nodes]):
+        print('Mismatch between nodes in DataFrame and nodes in graph')
+        return
+
     # extract indices of node attributes in tensor
     node_idx = [g.mapping.to_idx(x) for x in df['v']]
     for attr in df.columns:
@@ -150,22 +148,31 @@ def add_node_attributes(df: pd.DataFrame, g: Graph):
             g.data[attr] = df[attr].values[node_idx]
 
 
-def add_edge_attributes(df: pd.DataFrame, g: Graph):
+def add_edge_attributes(df: pd.DataFrame, g: Graph) -> None:
     """Add edge attributes from pandas data frame to existing graph, where source/target node
-    IDs or indices are given in columns `v` and `w`  and edge attributes x are given in columns `edge_x`
+    IDs are given in columns `v` and `w`  and edge attributes x are given in columns `edge_x`
     """
 
-    # TODO: handle cases where not all nodes have attributes
     if 'v' not in df or 'w' not in df:
         print('data frame must have columns `v` and `w`')
         return
-    if df.duplicated(subset=['v', 'w']).any():
-        print('data frame cannot contain multiple attribute values for single edge')
+    
+    attributed_edges = list(zip(df['v'], df['w']))
+
+    # check for duplicated edge attributes
+    if len(set(attributed_edges)) < len(attributed_edges):
+        print('data frame contains multiple attribute values for single edge')
+        return
+
+    # check for difference between edges in graph and edges in attributes
+    if set(attributed_edges) != set([(v,w) for v,w in g.edges]):
+        print('Mismatch between edges in DataFrame and edges in graph')
         return
 
     # extract indices of source/target node of edges
     src = [g.mapping.to_idx(x) for x in df['v']]
     tgt = [g.mapping.to_idx(x) for x in df['w']]
+
     # find indices of edges in edge_index
     edge_idx = []
     for i in range(len(src)):
@@ -176,7 +183,10 @@ def add_edge_attributes(df: pd.DataFrame, g: Graph):
             g.data[attr] = df[attr].values[edge_idx]
 
 
-def df_to_temporal_graph(df: pd.DataFrame, timestamp_format='%Y-%m-%d %H:%M:%S', time_rescale=1,
+def df_to_temporal_graph(df: pd.DataFrame,
+                         is_undirected: bool = False,
+                         timestamp_format='%Y-%m-%d %H:%M:%S', 
+                         time_rescale=1,
                         **kwargs: Any) -> TemporalGraph:
     """Reads a temporal graph from a pandas data frame.
 
@@ -209,27 +219,49 @@ def df_to_temporal_graph(df: pd.DataFrame, timestamp_format='%Y-%m-%d %H:%M:%S',
         't': [1, 2, 3]})
     g = pp.io.df_to_temporal_graph(df)
     print(g)
+
+    df = pd.DataFrame([
+        ['a', 'b', 'c'],
+        ['b', 'c', 'a'],
+        [1, 2, 3]
+        ])
+    g = pp.io.df_to_temporal_graph(df)
+    print(g)
     ```
     """
+    # assign column names if no header is present
+    no_header = all(isinstance(x, int) for x in df.columns.values.tolist())
+
+    if no_header:
+        # interpret first two columns as source and target
+        col_names = ['v', 'w', 't']
+        # interpret remaining columns as edge attributes
+        for i in range(3, len(df.columns.values.tolist())):
+            col_names += ['edge_attr_{0}'.format(i-2)]
+        df.columns = col_names
+
     tedges = []
     for row in df.to_dict(orient='records'):
         _v, _w, _t = str(row.pop('v')), str(row.pop('w')), str(row.pop('t'))
         try:
             t = float(_t)
         except:
-            # if it is a string, we use the timestamp format to convert
-            # it to a UNIX timestamp
+            # if time stamp is a string, use timestamp_format to convert
+            # it to UNIX timestamp
             x = datetime.datetime.strptime(_t, timestamp_format)
             t = int(mktime(x.timetuple()))
         tedges.append((_v, _w, int(t/time_rescale)))
 
     g = TemporalGraph.from_edge_list(tedges)
+    
+    if is_undirected:
+        return g.to_undirected()
+    else:
+        return g
 
-    return g
 
-
-def to_df(graph: Union[Graph, TemporalGraph], node_indices: Optional[bool] = False) -> pd.DataFrame:
-    """Returns a pandas dataframe of a given graph.
+def graph_to_df(graph: Graph, node_indices: Optional[bool] = False) -> pd.DataFrame:
+    """Returns a pandas data frame for a given graph.
 
     Returns a pandas dataframe data that contains all edges including edge
     attributes. Node and network-level attributes are not included. To
@@ -248,6 +280,40 @@ def to_df(graph: Union[Graph, TemporalGraph], node_indices: Optional[bool] = Fal
     n = pp.Graph.from_edge_list([('a', 'b'), ('b', 'c'), ('c', 'a')])
     df = pp.io.to_dataframe(n)
     print(df)
+    ```
+    """
+    df = pd.DataFrame()
+
+    for v, w in graph.edges:
+        if node_indices:
+            v = graph.mapping.to_idx(v)
+            w = graph.mapping.to_idx(w)
+        edge_frame = pd.DataFrame.from_dict({'v': [v], 'w': [w]})
+        df = pd.concat([df, edge_frame], ignore_index=True, sort=False)
+
+    edge_attribute_df = pd.DataFrame.from_dict(
+        {a: graph.data[a] for a in graph.edge_attrs()})
+    df = pd.concat([df, edge_attribute_df], axis=1)
+    return df
+
+
+def temporal_graph_to_df(graph: TemporalGraph, 
+                         node_indices: Optional[bool] = False) -> pd.DataFrame:
+    """Returns a pandas data frame for a given temporal graph.
+
+    Returns a pandas dataframe data that contains all edges including edge
+    attributes. Node and network-level attributes are not included. To
+    facilitate the import into network analysis tools that only support integer
+    node identifiers, node uids can be replaced by a consecutive, zero-based
+    index.
+
+    Args:
+        graph: The graph to export as pandas DataFrame
+        node_indices: whether nodes should be exported as integer indices
+
+    Example:
+    ```py
+    import pathpyG as pp
     
     n = pp.TemporalGraph.from_edge_list([('a', 'b', 1), ('b', 'c', 2), ('c', 'a', 3)])
     df = pp.io.to_df(n)
@@ -257,39 +323,24 @@ def to_df(graph: Union[Graph, TemporalGraph], node_indices: Optional[bool] = Fal
     df = pd.DataFrame()
 
     # export temporal graph
-    if isinstance(graph, TemporalGraph):
-        for v, w, t in graph.temporal_edges:
-            if node_indices:
-                v = graph.mapping.to_idx(v)
-                w = graph.mapping.to_idx(w)
-            edge_frame = pd.DataFrame.from_dict({'v': [v], 'w': [w], 't': [t]})
-            #data = pd.DataFrame.from_dict(
-            #    {k: [v] for k, v in edge.attributes.items()})
-            #edge_frame = pd.concat([edge_frame, data], axis=1)
-            df = pd.concat([edge_frame, df], ignore_index=True, sort=False)
-    else:
-        # export static graph
-        for v, w in graph.edges:
-            if node_indices:
-                v = graph.mapping.to_idx(v)
-                w = graph.mapping.to_idx(w)
-            edge_frame = pd.DataFrame.from_dict({'v': [v], 'w': [w]})
-            #data = pd.DataFrame.from_dict(
-            #    {k: [v] for k, v in edge.attributes.items()})
-            #edge_frame = pd.concat([edge_frame, data], axis=1)
-            df = pd.concat([edge_frame, df], ignore_index=True, sort=False)
+    for v, w, t in graph.temporal_edges:
+        if node_indices:
+            v = graph.mapping.to_idx(v)
+            w = graph.mapping.to_idx(w)
+        edge_frame = pd.DataFrame.from_dict({'v': [v], 'w': [w], 't': [t]})
+        #data = pd.DataFrame.from_dict(
+        #    {k: [v] for k, v in edge.attributes.items()})
+        #edge_frame = pd.concat([edge_frame, data], axis=1)
+        df = pd.concat([edge_frame, df], ignore_index=True, sort=False)
     return df
 
 
-def read_csv(filename: str,
-                 loops: bool = True,
-                 directed: bool = True,
-                 multiedges: bool = False,
-                 sep: str = ',',
-                 header: bool = True,
-                 timestamp_format: str = '%Y-%m-%d %H:%M:%S',
-                 time_rescale: int = 1,
-                 **kwargs: Any) -> Union[Graph,TemporalGraph]:
+def read_csv_graph(filename: str,
+                sep: str = ',',
+                header: bool = True,
+                is_undirected: bool = False,
+                multiedges: bool = False,
+                **kwargs: Any) -> Graph:
     """Reads a Graph or TemporalGraph from a csv file. To read a temporal graph, the csv file must have
     a header with column `t` containing time stamps of edges
     
@@ -311,13 +362,42 @@ def read_csv(filename: str,
         ```
     """
     if header:
-        df = pd.read_csv(filename, sep=sep)
-    else:
         df = pd.read_csv(filename, header=0, sep=sep)
-    if 't' in df:
-        return df_to_temporal_graph(df, timestamp_fromat=timestamp_format, time_rescale=time_rescale, **kwargs)
     else:
-        return df_to_graph(df, loops=loops, directed=directed, multiedges=multiedges, **kwargs)
+        df = pd.read_csv(filename, header=None, sep=sep)
+
+    return df_to_graph(df, is_undirected=is_undirected, multiedges=multiedges, **kwargs)
+
+
+def read_csv_temporal_graph(filename: str,
+                 sep: str = ',',
+                 header: bool = True,
+                 is_undirected: bool = True,
+                 timestamp_format: str = '%Y-%m-%d %H:%M:%S',
+                 time_rescale: int = 1,
+                 **kwargs: Any) -> TemporalGraph:
+    """Reads a TemporalGraph from a csv file that minimally has three columns
+    containin source, target and time.
+    
+    Args:
+        sep: character separating columns in the csv file
+        header: whether or not the first line of the csv file is interpreted as header with column names
+        directed: whether or not to intepret edges as directed
+        timestamp_format: format of timestamps
+        time_rescale: rescaling of timestamps
+
+    Example:
+        ```py
+        import pathpyG as pp
+
+        g = pp.io.read_csv('example_temporal_graph.csv')
+        ```
+    """
+    if header:
+        df = pd.read_csv(filename, header=0, sep=sep)
+    else:
+        df = pd.read_csv(filename, header=None, sep=sep)
+    return df_to_temporal_graph(df, is_undirected=is_undirected, timestamp_fromat=timestamp_format, time_rescale=time_rescale, **kwargs)
 
 
 def write_csv(graph: Union[Graph, TemporalGraph],
@@ -325,5 +405,8 @@ def write_csv(graph: Union[Graph, TemporalGraph],
           node_indices: bool = False,
           **pdargs: Any) -> None:
     """Stores all edges including edge attributes in a csv file."""
-    frame = to_df(graph=graph, node_indices=node_indices)
+    if isinstance(graph, TemporalGraph):
+        frame = temporal_graph_to_df(graph=graph, node_indices=node_indices)
+    else:
+        frame = graph_to_df(graph=graph, node_indices=node_indices)
     frame.to_csv(path_or_buf=path_or_buf, index=False, **pdargs)
--- tests/io/conftest.py
+++ tests/io/conftest.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 @pytest.fixture
 def df_graph() -> pd.DataFrame:
-    """Return a DataFrame for a simple graph."""
+    """DataFrame for simple graph with header and no edge attributes."""
     df = pd.DataFrame({
         'v': ['a', 'b', 'c'],
         'w': ['b', 'c', 'a']})
@@ -15,17 +15,22 @@ def df_graph() -> pd.DataFrame:
 
 @pytest.fixture
 def df_graph_attribute() -> pd.DataFrame:
-    """Return a DataFrame for a simple attributed graph."""
+    """DataFrame for simple graph with edge attributes and header."""
     df = pd.DataFrame({
         'v': ['a', 'b', 'c'],
         'w': ['b', 'c', 'a'],
-        'weight': [2.0, 1.0, 42.0]})
+        'edge_weight': [2.0, 1.0, 42.0]})
     return df
 
+@pytest.fixture
+def df_graph_attribute_no_header() -> pd.DataFrame:
+    """DataFrame for simple graph with edge attributes and no header."""
+    df = pd.DataFrame([['a', 'b', 2.0], ['b', 'c', 1.0], ['c', 'a', 42.0]])
+    return df
 
 @pytest.fixture
 def df_temporal_graph() -> pd.DataFrame:
-    """Return a DataFrame for a simple temporal graph."""
+    """DataFrame for simple temporal graph with header."""
     df = pd.DataFrame({
         'v': ['a', 'b', 'c'],
         'w': ['b', 'c', 'a'],
@@ -33,3 +38,8 @@ def df_temporal_graph() -> pd.DataFrame:
     return df
 
 
+@pytest.fixture
+def df_temporal_graph_no_header() -> pd.DataFrame:
+    """DataFrame for simple temporal graph without header."""
+    df = pd.DataFrame([['a', 'b', 1], ['b', 'c', 2], ['c', 'a', 3]])
+    return df
