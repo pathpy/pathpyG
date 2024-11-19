1bde2efa056e0819686e729f66c4076c96f5e601
Jürgen Hackl <hackl@users.noreply.github.com>
2024-11-19 04:51:58 -0500

    229 enhance config handling (#237)
    * changed config file handling (#229)
    
    * add tomllib to dependencies
    
    * fix module name
    
    * implement workaround for py > 3.11

--- src/pathpyG/utils/config.py
+++ src/pathpyG/utils/config.py
@@ -1,86 +1,39 @@
-#!/usr/bin/python -tt
-# -*- coding: utf-8 -*-
-# =============================================================================
-# File      : config.py -- Module to read and parse configuration files
-# Author    : Jürgen Hackl <hackl@ifi.uzh.ch>
-# Time-stamp: <Wed 2021-04-21 10:00 juergen>
-#
-# Copyright (c) 2016-2019 Pathpy Developers
-# =============================================================================
+"""Config reader."""
 
 import os
-from configparser import ConfigParser
-from collections import defaultdict
+import sys
 
-__all__ = ["config"]
+# Official workaround: https://github.com/hukkin/tomli
+if sys.version_info >= (3, 11):
+    import tomllib
+else:
+    import tomli as tomllib
 
-# get pathpy base config
-_base_config = os.path.join(os.path.dirname(os.path.dirname(__file__)), "config.cfg")
-
-# setup parser
-parser = ConfigParser()
-
-# load config files
-parser.read([_base_config, "config.cfg"])
-
-# setup parser
-config: dict = defaultdict(dict)
-
-config["environment"]["IDE"] = parser.get("environment", "IDE")
-config["environment"]["interactive"] = parser.getboolean("environment", "interactive")
-
-# TODO: Find a better way to load the config file.
-config["logging"]["enabled"] = parser.getboolean("logging", "enabled")
-config["logging"]["verbose"] = parser.getboolean("logging", "verbose")
-config["logging"]["level"] = parser.get("logging", "level")
-
-config["progress"]["enabled"] = parser.getboolean("progress", "enabled")
-config["progress"]["min_iter"] = parser.getint("progress", "min_iter")
-config["progress"]["leave"] = parser.getboolean("progress", "leave")
-
-config["attributes"]["history"] = parser.getboolean("attributes", "history")
-config["attributes"]["multiple"] = parser.getboolean("attributes", "multiple")
-config["attributes"]["frequency"] = parser.get("attributes", "frequency")
+import pip._vendor.tomli as tomllib
+from pathlib import Path
 
 
-config["torch"]["device"] = parser.get("torch", "device")
+# path to the module
+path = Path(sys.modules[__name__].__file__).resolve().parents[1]
 
-config["object"]["separator"] = parser.get("object", "separator")
+# default config file name
+configfile_name = "pathpyG.toml"
 
-config["node"]["color"] = parser.get("node", "color")
-config["node"]["size"] = parser.getint("node", "size")
+# load default config
+configfile_path = os.path.join(path, configfile_name)
 
-config["edge"]["color"] = parser.get("edge", "color")
-config["edge"]["width"] = parser.getint("edge", "width")
-config["edge"]["curved"] = parser.getboolean("edge", "curved")
+# load config file
+with open(configfile_path, "rb") as f:
+    config = tomllib.load(f)
 
+# check if local config file is defined
+if os.path.exists(os.path.join(os.getcwd(), configfile_name)):
+    # get location of local config file
+    configfile_path = os.path.join(os.getcwd(), configfile_name)
 
-config["path"]["separator"] = parser.get("path", "separator")
-config["path"]["replace"] = parser.get("path", "replace")
-config["path"]["max_name_length"] = parser.getint("path", "max_name_length")
-config["hon"]["separator"] = parser.get("hon", "separator")
-config["hon"]["replace"] = parser.get("hon", "replace")
+    # load local config file
+    with open(configfile_path, "rb") as f:
+        _config = tomllib.load(f)
 
-# config['temporal']['begin'] = parser.get('temporal', 'begin')
-config["temporal"]["start"] = parser.get("temporal", "start")
-config["temporal"]["end"] = parser.get("temporal", "end")
-config["temporal"]["timestamp"] = parser.get("temporal", "timestamp")
-config["temporal"]["duration"] = parser.get("temporal", "duration")
-config["temporal"]["duration_value"] = parser.getfloat("temporal", "duration_value")
-config["temporal"]["active"] = parser.get("temporal", "active")
-config["temporal"]["is_active"] = parser.getboolean("temporal", "is_active")
-config["temporal"]["unit"] = parser.get("temporal", "unit")
-config["temporal"]["start_synonyms"] = parser.get("temporal", "start_synonyms").split(", ")
-config["temporal"]["end_synonyms"] = parser.get("temporal", "end_synonyms").split(", ")
-config["temporal"]["timestamp_synonyms"] = parser.get("temporal", "timestamp_synonyms").split(", ")
-config["temporal"]["duration_synonyms"] = parser.get("temporal", "duration_synonyms").split(", ")
-
-# =============================================================================
-# eof
-#
-# Local Variables:
-# mode: python
-# mode: linum
-# mode: auto-fill
-# fill-column: 79
-# End:
+    # update default config file
+    config.update(_config)
