a885b2507c989d1d305256a9ec599dee796bfdc2
Leonie <you@example.com>
2025-05-18 13:37:14 +0000

    Enable saving to custom or current directory

--- src/pathpyG/visualisations/_manim/core.py
+++ src/pathpyG/visualisations/_manim/core.py
@@ -1,16 +1,14 @@
 """Generic manim plot class."""
 
 from __future__ import annotations
-
 import logging
-
 from typing import Any
-
+from pathlib import Path
+from datetime import datetime
 from pathpyG.visualisations.plot import PathPyPlot
 
 # create logger
 logger = logging.getLogger("root")
-
 
 
 class ManimPlot(PathPyPlot):
@@ -20,20 +18,20 @@ class ManimPlot(PathPyPlot):
         """Generate the plot."""
         raise NotImplementedError
 
-    def save(self, filename: str, **kwargs: Any) -> None:
-
+    def save(self, **kwargs: Any) -> None:
         raise NotImplementedError
 
-    def show(self, **kwargs: Any) -> None:
-        """renders manim scene to current directory"""
-        from manim import config
+    def show(self, *, output_dir: str | Path = None, output_file: str = None, **kwargs: Any) -> None:
+        """
+        This function generates and saves a Manim Plot
+        """
 
-        config.pixel_height = 1080
-        config.pixel_width = 1920
-        config.frame_rate = 30
-        config.quality = "medium_quality"
-        config.output_file = kwargs.get("output_file", None)
-        
-        self.render()
-        
+        output_dir = Path(output_dir or Path.cwd())
+        output_dir.mkdir(parents=True, exist_ok=True)
 
+        if output_file is None:
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            output_file = f"{self.__class__.__name__}_{timestamp}"
+
+        scene = self.__class__(data=self.raw_data, output_dir=output_dir, output_file=output_file, **kwargs)
+        scene.render()
