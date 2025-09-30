259bcf78a4377e15430aa7335274518d34c30b30
Moritz Lampert <moritz@lampert.ai>
2025-09-30 09:16:56 +0000

    fix test shortest paths

--- tests/algorithms/test_shortest_paths.py
+++ tests/algorithms/test_shortest_paths.py
@@ -2,17 +2,16 @@ from __future__ import annotations
 
 import numpy as _np
 
-from pathpyG.core.graph import Graph
-from pathpyG.visualisations.hist_plots import hist
-from pathpyG.algorithms import centrality
 from pathpyG.algorithms.shortest_paths import shortest_paths_dijkstra, diameter, avg_path_length
 
 
 def test_shortest_paths_dijkstra(simple_graph_sp):
     dist, pred = shortest_paths_dijkstra(simple_graph_sp)
-    print(pred)
     assert (dist == _np.matrix("0 1 2 2 3; 1 0 1 1 2; 2 1 0 2 1; 2 1 2 0 1; 3 2 1 1 0").A).all()
-    assert (pred == _np.matrix("-9999 0 1 1 2; 1 -9999 1 1 3; 1 2 -9999 1 2; 1 3 1 -9999 3; 1 2 4 4 -9999").A).all()
+    for i in range(dist.shape[0]):
+        for j in range(dist.shape[1]):
+            if i != j:
+                assert dist[i, j] == dist[i, pred[i, j]] + 1
 
 
 def test_diameter(simple_graph_sp):
